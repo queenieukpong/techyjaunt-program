4f5589f1f818a66ddfca175d3ead266c715bfd99
InyangUkpong <queenieukpong@outlook.com>
2024-04-02 00:27:21 +0100

    this is a first request from boujeenerd

---
+++ BoujeenerdQ/newEmployee.js
@@ -0,0 +1,20 @@
+const createEmployee = (name, role, salary) => {
+    return { name, role, salary };
+};
+
+let Janet = createEmployee("Janet", "frontend dev", 2000);
+
+const getSalary = (employee) => {
+    return `The salary of ${employee.name} is ${employee.salary} per day`;
+};
+
+console.log(getSalary(Janet));
+
+// CREATE A NEW FUNCTION THAT UPDATES EMPLOYEE SALARY
+
+const updateSalary = (employee) => {
+    employee.salary = newSalary;
+    return `Mrs, ${employee.name}'s new salary is ${employee.salary} per month`;
+}
+
+console.log(updateSalary(Janet, 4200));
